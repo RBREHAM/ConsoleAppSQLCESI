ae1b1032ba987b9e252e83682390a8b2b3adebe4
RAPHAEL BREHAM <raphael.breham@viacesi.fr>
2023-05-01 18:07:22 +0200

    feat(requests) : Show of SQL request in console finished

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
 ﻿using System.Data.SqlClient;
 
+namespace SQLWork;
 class Program
 {
     static void Main(string[] args)
     {
 
+        SqlConnection connection = new SqlConnection();
+        connection.ConnectionString = @"Data Source = ISIITECH69\SQLEXPRESS; Initial Catalog = booksdb; Integrated Security = True";
+        connection.Open();
 
+        string query = "Select * FROM Books";
+        SqlCommand command = new SqlCommand(query, connection);
+        SqlDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine(reader["id"] + " " + reader["Title"] + " " + reader["Year"] + " " + reader["Category"]);
+        }
+
+        reader.Close();
+        connection.Close();
     }
 
 }
